03205a9b0a1ec9c23fa90a185d20e463340f4ea5
pankaj pal <papal@microsoft.com>
2020-07-28 15:39:31 +0530

    making routing work

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'public')));
-const port = process.env.PORT || '3001';
+
+const port = process.env.PORT || '3002';
 app.set('port', port);
+
+app.get('/', function(req, res){
+	console.log('m here');
+	res.sendFile(path.join(__dirname+'/public/taskpane.html'));
+})
+
 const server = http.createServer(app);
 server.listen(port, () => {
 	console.log("Listening on port " + port);
