985e39f8de0af876e865fdba54a63a3dd2d42354
pankaj pal <papal@microsoft.com>
2020-07-29 09:56:21 +0530

    updating initial card

--- public/card.js
+++ public/card.js
@@ -1,65 +1,90 @@
 var initialHelpCard = {
+    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
     "type": "AdaptiveCard",
-    "id": "427FB9DB-E2E6-4A26-B684-454D6B62A731",
-    "correlationId": "9b7aedb0-833f-4bde-b9b4-31f69ca14dc0",
-    "originator": "0eb3a855-e2d4-4bc9-8038-b22d614e4788",
     "version": "1.0",
-    "padding": "Default",
     "body": [
         {
-            "text": "What can we help you with?",
-            "wrap": true,
-            "id": "3095c4b4-03a5-dd07-7915-98d7c7fef7a9",
-            "type": "TextBlock"
+            "type": "Container",
+            "id": "6343565f-52eb-c87b-c815-b772a86f1aca",
+            "padding": "Default",
+            "items": [
+                {
+                    "type": "TextBlock",
+                    "id": "d9c56323-fe1a-e090-1df5-311743b856cd",
+                    "text": "Actionable Messages Support Team",
+                    "wrap": true
+                }
+            ],
+            "spacing": "None",
+            "style": "emphasis"
         },
         {
-            "actions": [
+            "type": "Container",
+            "id": "03a273b0-e511-5f0d-3a44-1eddaa03a8a5",
+            "padding": "Default",
+            "spacing": "None",
+            "items": [
                 {
-                    "method": "POST",
-                    "url": "https://amsupporthack.azurewebsites.net/api/PartnerOnboarding?code=9d97sV8aptFv/CuPd7iObLvG52jhAUFNXT4allUzmXBkNmwW3uQI7w==",
-                    "title": "Partner Onboarding",
-                    "isPrimary": false,
-                    "type": "Action.Http",
-                    "id": "4d1029b7-2d80-8088-8f6f-48532e1b286f"
-                },
+                    "type": "TextBlock",
+                    "id": "231d74e7-5c53-6ec8-102d-718dc936bc09",
+                    "text": "What can we help you with today?",
+                    "wrap": true,
+                    "size": "Large",
+                    "weight": "Bolder"
+                }
+            ]
+        },
+        {
+            "type": "Container",
+            "id": "885220a9-5ab1-95dd-5b66-20f42c452fa9",
+            "padding": "Default",
+            "items": [
                 {
-                    "method": "POST",
-                    "url": "https://amsupporthack.azurewebsites.net/api/Rendering?code=dEW2WK7jALVQeBXBxVb3tg2dRPWal2Tzr9OQYbL4YCzN0wJaJ/Rshw==",
-                    "title": "Rendering",
-                    "isPrimary": false,
-                    "type": "Action.Http",
-                    "id": "3efd62b1-8fb9-b965-6fcc-a7a4491ee841"
-                },
-                {
-                    "method": "POST",
-                    "url": "https://amsupporthack.azurewebsites.net/api/ActionExecution?code=vitS0zijbtoiqowrATHYHSm6er/3ASJozT0GoNZTHyMfBtKcGLZyPA==",
-                    "title": "Action Execution",
-                    "isPrimary": false,
-                    "type": "Action.Http",
-                    "id": "6487a969-45cf-071f-f2a4-6052e46c16e8"
-                },
-                {
-                    "method": "POST",
-                    "url": "https://amsupporthack.azurewebsites.net/api/Others?code=QRdK6GiHRrv/96q0IOKLlboan89spKuS7P0TIjTIT0KO/ssQbZFx8Q==",
-                    "title": "Others",
-                    "isPrimary": false,
-                    "type": "Action.Http",
-                    "id": "6487a969-45cf-071f-f2a4-6052e46c16e8"
-                },
-                {
-					"type": "Action.DisplayMessageForm",
-					"itemId": "abcdef",
-					"title": "Email us"
-				}
+                    "actions": [
+                        {
+                            "method": "POST",
+                            "url": "https://amsupporthack.azurewebsites.net/api/PartnerOnboarding?code=9d97sV8aptFv/CuPd7iObLvG52jhAUFNXT4allUzmXBkNmwW3uQI7w==",
+                            "title": "Partner Onboarding",
+                            "isPrimary": false,
+                            "type": "Action.Http",
+                            "id": "4d1029b7-2d80-8088-8f6f-48532e1b286f"
+                        },
+                        {
+                            "method": "POST",
+                            "url": "https://amsupporthack.azurewebsites.net/api/Rendering?code=dEW2WK7jALVQeBXBxVb3tg2dRPWal2Tzr9OQYbL4YCzN0wJaJ/Rshw==",
+                            "title": "Rendering",
+                            "isPrimary": false,
+                            "type": "Action.Http",
+                            "id": "3efd62b1-8fb9-b965-6fcc-a7a4491ee841"
+                        },
+                        {
+                            "method": "POST",
+                            "url": "https://amsupporthack.azurewebsites.net/api/ActionExecution?code=vitS0zijbtoiqowrATHYHSm6er/3ASJozT0GoNZTHyMfBtKcGLZyPA==",
+                            "title": "Action Execution",
+                            "isPrimary": false,
+                            "type": "Action.Http",
+                            "id": "6487a969-45cf-071f-f2a4-6052e46c16e8"
+                        },
+                        {
+                            "method": "POST",
+                            "url": "https://amsupporthack.azurewebsites.net/api/Others?code=QRdK6GiHRrv/96q0IOKLlboan89spKuS7P0TIjTIT0KO/ssQbZFx8Q==",
+                            "title": "Others",
+                            "isPrimary": false,
+                            "type": "Action.Http",
+                            "id": "6487a969-45cf-071f-f2a4-6052e46c16e8"
+                        },
+                        {
+                            "type": "Action.DisplayMessageForm",
+                            "itemId": "abcdef",
+                            "title": "Email us"
+                        }
+                    ],
+                    "id": "1097f031-1e62-fa96-9010-0ef9553f2068",
+                    "type": "ActionSet"
+                }
             ],
-            "id": "1097f031-1e62-fa96-9010-0ef9553f2068",
-            "type": "ActionSet"
+            "spacing": "None"
         }
     ],
-    "autoInvokeAction": null,
-    "autoInvokeOptions": null,
-    "constrainWidth": true,
-    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-    "@type": "AdaptiveCard",
-    "@context": "http://schema.org/extensions"
+    "padding": "None"
 };
